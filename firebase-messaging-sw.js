409de84f1087e2ae0c5627dcd3f04bfafda7bcc0
amol632 <amolwashimkar660@gmail.com>
2025-09-16 09:04:48 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCiFiazAT4lummi9MJTkDOU_ZhpgeBHXGU",
+  authDomain: "agrimitra-6b944.firebaseapp.com",
+  projectId: "agrimitra-6b944",
+  storageBucket: "agrimitra-6b944.firebasestorage.app",
+  messagingSenderId: "190603132023",
+  appId: "1:190603132023:web:dc3f122284891e180f9a72",
+  measurementId: "G-7MXB81G9PW"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("📩 Background message received:", payload);
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
