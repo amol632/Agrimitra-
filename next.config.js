da58c5199f045934c69c58b2615f4a9c08a0ff61
amol632 <amolwashimkar660@gmail.com>
2025-12-13 16:54:04 +0530

    Update and rename next.config.js to next.config.mjs

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// ✅ हा नवीन आणि योग्य कोड वापरा
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig; // 'module.exports' ऐवजी 'export default' वापरा
-
---
+++ next.config.mjs
@@ -0,0 +1,5 @@
+// ✅ next.config.mjs मधील कोड
+/** @type {import('next').NextConfig} */
+const nextConfig = {}; // तुमच्या सेटिंग्ज रिकाम्या राहू द्या
+
+export default nextConfig; // 'module.exports' अजिबात वापरू नका
