75e6929be329223782e6f87520d6a543e09b057f
amol632 <amolwashimkar660@gmail.com>
2025-12-13 10:19:52 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ही सेटिंग Next.js ला सांगते की, UI साठी फक्त Static HTML वापरावा.
+  output: 'export',
+};
+
+module.exports = nextConfig;
+
